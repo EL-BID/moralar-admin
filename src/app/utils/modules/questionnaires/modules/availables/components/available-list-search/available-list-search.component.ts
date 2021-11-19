f400e58c8b57000c626f37f951b3c392ae031607
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-11-19 16:06:12 -0300

    Correção de lógica

--- src/app/utils/modules/questionnaires/modules/availables/components/available-list-search/available-list-search.component.ts
+++ src/app/utils/modules/questionnaires/modules/availables/components/available-list-search/available-list-search.component.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
-import { ListSearchComponentClass } from 'src/app/utils/classes/list-search-component.class';
 import { FormBuilder } from '@angular/forms';
+import { quizTypeStatus } from 'src/app/core/mocks/quizTypeStatus';
+import { ListSearchComponentClass } from 'src/app/utils/classes/list-search-component.class';
 
 @Component({
   selector: 'app-available-list-search',
   templateUrl: './available-list-search.component.html',
-  styleUrls: ['./available-list-search.component.sass']
+  styleUrls: ['./available-list-search.component.sass'],
 })
 export class AvailableListSearchComponent extends ListSearchComponentClass {
-
-  constructor(
-    formBuilder: FormBuilder
-  ) {
+  quizTypeStatus = quizTypeStatus;
+  constructor(formBuilder: FormBuilder) {
     super();
     this.listSearchForm = formBuilder.group({
       search: [''],
       number: [''],
       holderName: [''],
       holderCpf: [''],
-      status: ['']
+      status: [''],
     });
   }
-
 }
