3dafd73f65e09582d10452e2375f1f97edfe608a
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-11-25 16:06:34 -0300

    Alteração da máscara do telefone

--- src/app/modules/administrator/modules/private/modules/professionals/components/professional-form/professional-form.component.ts
+++ src/app/modules/administrator/modules/private/modules/professionals/components/professional-form/professional-form.component.ts
@@ -2,43 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 import { FormComponentClass } from 'src/app/utils/classes/form-component.class';
-import { isCpfValid, trimWhiteSpace } from 'src/app/utils/functions/validators.function';
+import {
+  isCpfValid,
+  trimWhiteSpace,
+} from 'src/app/utils/functions/validators.function';
 
 @Component({
   selector: 'app-professional-form',
   templateUrl: './professional-form.component.html',
-  styleUrls: ['./professional-form.component.sass']
+  styleUrls: ['./professional-form.component.sass'],
 })
-export class ProfessionalFormComponent extends FormComponentClass implements OnInit {
-
-  phoneMask: string;
-
-  constructor(
-    formBuilder: FormBuilder
-  ) {
+export class ProfessionalFormComponent
+  extends FormComponentClass
+  implements OnInit
+{
+  constructor(formBuilder: FormBuilder) {
     super();
     this.form = formBuilder.group({
-      name:        [null, Validators.compose([trimWhiteSpace, Validators.required])],
-      jobPost:     [null, Validators.compose([trimWhiteSpace, Validators.required])],
-      cpf:         [null, Validators.compose([Validators.required, isCpfValid])],
-      email:       [null, Validators.compose([trimWhiteSpace, Validators.required, Validators.email])],
-      phone:       [null],
-      typeProfile: ['TTS']
+      name: [null, Validators.compose([trimWhiteSpace, Validators.required])],
+      jobPost: [
+        null,
+        Validators.compose([trimWhiteSpace, Validators.required]),
+      ],
+      cpf: [null, Validators.compose([Validators.required, isCpfValid])],
+      email: [
+        null,
+        Validators.compose([
+          trimWhiteSpace,
+          Validators.required,
+          Validators.email,
+        ]),
+      ],
+      phone: [null],
+      typeProfile: ['TTS'],
     });
   }
 
-  ngOnInit(): void {
-    this.form.controls.phone.valueChanges
-      .pipe(takeUntil(this.onDestroy))
-      .subscribe((value: string) => {
-        if (typeof value === 'string') {
-          if (value.length === 11) {
-            this.phoneMask = '(00) 00000-0000';
-          } else {
-            this.phoneMask = '(00) 0000-00009';
-          }
-        }
-      });
-  }
-
+  ngOnInit(): void {}
 }
