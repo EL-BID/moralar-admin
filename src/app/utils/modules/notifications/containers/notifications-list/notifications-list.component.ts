622a56d23f8d1b01eeed9ae3676aec2e7dc26159
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-11-26 14:34:19 -0300

    Correção da URL das notificações e ocultação da listagem da mesma no menu superior

--- src/app/utils/modules/notifications/containers/notifications-list/notifications-list.component.ts
+++ src/app/utils/modules/notifications/containers/notifications-list/notifications-list.component.ts
@@ -3,38 +3,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ListContainerClass } from 'src/app/utils/classes/list-container.class';
 import { FormDataModel } from 'src/app/utils/functions/generate-form-data.function';
 import { HttpService } from 'src/app/utils/services/http/http.service';
-import {ModalConfirmData} from '../../../shared/components/modal-confirm/modal-confirm.interface';
-import {ModalConfirmComponent} from '../../../shared/components/modal-confirm/modal-confirm.component';
-import {takeUntil} from 'rxjs/operators';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {MegaleiosAlertService} from '../../../megaleios-alert/megaleios-alert.service';
+import { ModalConfirmData } from '../../../shared/components/modal-confirm/modal-confirm.interface';
+import { ModalConfirmComponent } from '../../../shared/components/modal-confirm/modal-confirm.component';
+import { takeUntil } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MegaleiosAlertService } from '../../../megaleios-alert/megaleios-alert.service';
 
 @Component({
   selector: 'app-notifications-list',
   templateUrl: './notifications-list.component.html',
-  styleUrls: ['./notifications-list.component.sass']
+  styleUrls: ['./notifications-list.component.sass'],
 })
 export class NotificationsListComponent extends ListContainerClass {
-
   formDataModel: FormDataModel = {
     columns: [
       { data: 'title', name: 'Title', searchable: true },
       { data: 'description', name: 'Description', searchable: true },
       { data: 'date', name: 'Date', searchable: true },
-      { data: 'status', name: 'Status', searchable: true }
+      { data: 'status', name: 'Status', searchable: true },
     ],
     page: 1,
     pageSize: 10,
     search: {
-      search: ''
+      search: '',
     },
     order: {
       column: '0',
-      direction: 'asc'
-    }
+      direction: 'asc',
+    },
   };
 
-  uri = 'Notification';
+  uriCustom = 'Notification/LoadDataNotification';
 
   constructor(
     activatedRoute: ActivatedRoute,
@@ -45,5 +44,4 @@ export class NotificationsListComponent extends ListContainerClass {
   ) {
     super(activatedRoute, router, httpService);
   }
-
 }
