8b3953d378528d4ee96b1b9d24197a13f1ddc3f8
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-02 17:45:44 -0300

    Uso do componente de data

--- src/app/modules/public-manager/modules/private/modules/schedules/components/schedule-list-search/schedule-list-search.component.ts
+++ src/app/modules/public-manager/modules/private/modules/schedules/components/schedule-list-search/schedule-list-search.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ListSearchComponentClass } from 'src/app/utils/classes/list-search-component.class';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { ListSearchComponentClass } from 'src/app/utils/classes/list-search-component.class';
 import {
   SCHEDULE_STATUS_LIST,
   SCHEDULE_TYPE_LIST,
 } from 'src/app/utils/interfaces/schedules.interface';
-import { cloneDeep } from 'lodash';
-import {
-  dateToSeconds,
-  dateToString,
-} from 'src/app/utils/functions/date.function';
-import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-schedule-list-search',
@@ -36,33 +30,5 @@ export class ScheduleListSearchComponent
       status: [''],
       type: [''],
     });
-  }
-
-  ngOnChanges(simpleChanges: SimpleChanges): void {
-    if (
-      simpleChanges &&
-      simpleChanges.listSearchData &&
-      simpleChanges.listSearchData.firstChange
-    ) {
-      const value = cloneDeep(simpleChanges.listSearchData.currentValue);
-      value.startDate = dateToString(value.startDate);
-      value.endDate = dateToString(value.endDate);
-      this.listSearchForm.patchValue(value);
-    }
-  }
-
-  ngOnInit(): void {
-    this.listSearchForm.valueChanges
-      .pipe(
-        takeUntil(this.onDestroy),
-        debounceTime(1000),
-        distinctUntilChanged()
-      )
-      .subscribe((value) => {
-        const valueFinal = cloneDeep(value);
-        value.startDate = dateToSeconds(value.startDate);
-        value.endDate = dateToSeconds(value.endDate);
-        this.listSearchChange.emit(value);
-      });
   }
 }
