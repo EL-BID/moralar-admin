d6c227c6f73a8aad6d7a330249d0f938864a227c
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-13 18:24:02 -0300

    Alteração da URL

--- src/app/utils/modules/families/components/family-list/family-list.component.ts
+++ src/app/utils/modules/families/components/family-list/family-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { AnyKindOfDictionary } from 'lodash';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
 
 @Component({
   selector: 'app-family-list',
   templateUrl: './family-list.component.html',
-  styleUrls: ['./family-list.component.sass']
+  styleUrls: ['./family-list.component.sass'],
 })
 export class FamilyListComponent extends ListComponentClass {
+  @Input() showItems!: AnyKindOfDictionary;
 
   @Output()
   blockUnblock: EventEmitter<any> = new EventEmitter();
@@ -16,8 +18,7 @@ export class FamilyListComponent extends ListComponentClass {
     this.blockUnblock.emit({
       targetId: value.target.value,
       block: !value.target.checked,
-      reason: null
+      reason: null,
     });
   }
-
 }
