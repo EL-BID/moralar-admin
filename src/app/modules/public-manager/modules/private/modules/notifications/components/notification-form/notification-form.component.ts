79863aa9e8db124164cff28e4bae2040947fbb70
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-13 16:21:53 -0300

    Teste de notificações

--- src/app/modules/public-manager/modules/private/modules/notifications/components/notification-form/notification-form.component.ts
+++ src/app/modules/public-manager/modules/private/modules/notifications/components/notification-form/notification-form.component.ts
@@ -1,22 +1,28 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormComponentClass } from 'src/app/utils/classes/form-component.class';
-import { FormBuilder, Validators} from '@angular/forms';
-import {FormDataModel, generateFormData} from '../../../../../../../../utils/functions/generate-form-data.function';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {takeUntil} from 'rxjs/operators';
-import {HttpService} from '../../../../../../../../utils/services/http/http.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import {
+  FormDataModel,
+  generateFormData,
+} from '../../../../../../../../utils/functions/generate-form-data.function';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { HttpService } from '../../../../../../../../utils/services/http/http.service';
 @Component({
   selector: 'app-notification-form',
   templateUrl: './notification-form.component.html',
-  styleUrls: ['./notification-form.component.sass']
+  styleUrls: ['./notification-form.component.sass'],
 })
-export class NotificationFormComponent extends FormComponentClass implements OnInit{
+export class NotificationFormComponent
+  extends FormComponentClass
+  implements OnInit
+{
   list: any[] = [];
   formDataModel: FormDataModel = {
     columns: [
-      { data: 'number',    name: 'Number',    searchable: true },
+      { data: 'number', name: 'Number', searchable: true },
       { data: 'holderName', name: 'HolderName', searchable: true },
-      { data: 'holderCpf',     name: 'SchedulesComponentCpf',     searchable: true }
+      { data: 'holderCpf', name: 'SchedulesComponentCpf', searchable: true },
     ],
     page: 1,
     pageSize: 10,
@@ -29,8 +35,8 @@ export class NotificationFormComponent extends FormComponentClass implements OnI
     },
     order: {
       column: '0',
-      direction: 'asc'
-    }
+      direction: 'asc',
+    },
   };
 
   constructor(
@@ -43,7 +49,7 @@ export class NotificationFormComponent extends FormComponentClass implements OnI
     super();
     this.form = this.formBuilder.group({
       familyId: [null],
-      allFamily: ['false', Validators.required],
+      allFamily: [false, Validators.required],
       image: [null],
       title: [null, Validators.required],
       description: [null, Validators.required],
@@ -51,31 +57,61 @@ export class NotificationFormComponent extends FormComponentClass implements OnI
   }
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams
-      .subscribe((params: Params) => {
-        this.formDataModel.search.search = params.search !== undefined ? params.search : this.formDataModel.search.search;
-        this.formDataModel.search.number = params.number !== undefined ? params.number : this.formDataModel.search.number;
-        this.formDataModel.search.holderName = params.holderName !== undefined ? params.holderName : this.formDataModel.search.holderName;
-        this.formDataModel.search.holderCpf = params.holderCpf !== undefined ? params.holderCpf : this.formDataModel.search.holderCpf;
-        this.formDataModel.search.status = params.status !== undefined ? params.status : this.formDataModel.search.status;
-        this.formDataModel.order.column = params.column !== undefined ? params.column : this.formDataModel.order.column;
-        this.formDataModel.order.direction = params.direction !== undefined ? params.direction : this.formDataModel.order.direction;
-        this.formDataModel.page = params.page !== undefined ? parseFloat(params.page) : this.formDataModel.page;
-        this.formDataModel.pageSize = params.pageSize !== undefined ? parseFloat(params.pageSize) : this.formDataModel.pageSize;
-        this.getList();
-      });
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      this.formDataModel.search.search =
+        params.search !== undefined
+          ? params.search
+          : this.formDataModel.search.search;
+      this.formDataModel.search.number =
+        params.number !== undefined
+          ? params.number
+          : this.formDataModel.search.number;
+      this.formDataModel.search.holderName =
+        params.holderName !== undefined
+          ? params.holderName
+          : this.formDataModel.search.holderName;
+      this.formDataModel.search.holderCpf =
+        params.holderCpf !== undefined
+          ? params.holderCpf
+          : this.formDataModel.search.holderCpf;
+      this.formDataModel.search.status =
+        params.status !== undefined
+          ? params.status
+          : this.formDataModel.search.status;
+      this.formDataModel.order.column =
+        params.column !== undefined
+          ? params.column
+          : this.formDataModel.order.column;
+      this.formDataModel.order.direction =
+        params.direction !== undefined
+          ? params.direction
+          : this.formDataModel.order.direction;
+      this.formDataModel.page =
+        params.page !== undefined
+          ? parseFloat(params.page)
+          : this.formDataModel.page;
+      this.formDataModel.pageSize =
+        params.pageSize !== undefined
+          ? parseFloat(params.pageSize)
+          : this.formDataModel.pageSize;
+      this.getList();
+    });
   }
   getFamily(e) {
     this.form.value.familyId = null;
-    this.router.navigate([], { relativeTo: this.activatedRoute, queryParams: e, queryParamsHandling: 'merge' });
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: e,
+      queryParamsHandling: 'merge',
+    });
   }
 
   getList(): void {
-    this.httpService.post(`Family/LoadData`, generateFormData(this.formDataModel))
+    this.httpService
+      .post(`Family/LoadData`, generateFormData(this.formDataModel))
       .pipe(takeUntil(this.onDestroy))
       .subscribe((response: any) => {
         this.list = response.data;
       });
   }
-
 }
