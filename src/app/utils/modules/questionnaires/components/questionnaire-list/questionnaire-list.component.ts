309e89bba55d3e9c96620229a072c2c46673ba0b
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-14 11:54:00 -0300

    Adição de funcionalidade

--- src/app/utils/modules/questionnaires/components/questionnaire-list/questionnaire-list.component.ts
+++ src/app/utils/modules/questionnaires/components/questionnaire-list/questionnaire-list.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
 
 @Component({
   selector: 'app-questionnaire-list',
   templateUrl: './questionnaire-list.component.html',
-  styleUrls: ['./questionnaire-list.component.sass']
+  styleUrls: ['./questionnaire-list.component.sass'],
 })
-export class QuestionnaireListComponent extends ListComponentClass { }
+export class QuestionnaireListComponent extends ListComponentClass {
+  @Output()
+  blockUnblock: EventEmitter<any> = new EventEmitter();
+
+  handleBlockUnblock(value: any): void {
+    value.preventDefault();
+    this.blockUnblock.emit({
+      targetId: value.target.value,
+      block: !value.target.checked,
+      reason: null,
+    });
+  }
+}
--- src/app/utils/modules/questionnaires/containers/questionnaires-list/questionnaires-list.component.ts
+++ src/app/utils/modules/questionnaires/containers/questionnaires-list/questionnaires-list.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { takeUntil } from 'rxjs/operators';
 import { ListContainerClass } from 'src/app/utils/classes/list-container.class';
 import { FormDataModel } from 'src/app/utils/functions/generate-form-data.function';
 import { HttpService } from 'src/app/utils/services/http/http.service';
+import { MegaleiosAlertService } from '../../../megaleios-alert/megaleios-alert.service';
+import { ModalConfirmComponent } from '../../../shared/components/modal-confirm/modal-confirm.component';
+import { ModalConfirmData } from '../../../shared/components/modal-confirm/modal-confirm.interface';
 
 @Component({
   selector: 'app-questionnaires-list',
   templateUrl: './questionnaires-list.component.html',
-  styleUrls: ['./questionnaires-list.component.sass']
+  styleUrls: ['./questionnaires-list.component.sass'],
 })
 export class QuestionnairesListComponent extends ListContainerClass {
-
   formDataModel: FormDataModel = {
-    columns: [
-      { data: 'title', name: 'Title', searchable: true }
-    ],
+    columns: [{ data: 'title', name: 'Title', searchable: true }],
     page: 1,
     pageSize: 10,
     search: {
@@ -23,26 +25,71 @@ export class QuestionnairesListComponent extends ListContainerClass {
       holderName: '',
       holderCpf: '',
       status: '',
-      typeQuiz: 0
+      typeQuiz: 0,
     },
     order: {
       column: '0',
-      direction: 'asc'
-    }
+      direction: 'asc',
+    },
   };
 
   uri = 'Quiz';
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private router: Router,
-    httpService: HttpService
+    activatedRoute: ActivatedRoute,
+    router: Router,
+    httpService: HttpService,
+    private ngbModal: NgbModal,
+    private megaleiosAlertService: MegaleiosAlertService
   ) {
     super(activatedRoute, router, httpService);
   }
 
   handleDetails(): void {
-    this.router.navigate([this.listSelected[0].id], { relativeTo: this.activatedRoute });
+    this._router.navigate([this.listSelected[0].id], {
+      relativeTo: this._activatedRoute.parent,
+    });
   }
 
+  handleBlockUnblock(value: any): void {
+    let modalConfirmData: ModalConfirmData;
+    if (value.block) {
+      modalConfirmData = {
+        title: 'Inativar questionário',
+        content: 'Deseja realmente inativar esse questionário?',
+        action: 'inativar',
+      };
+    } else {
+      modalConfirmData = {
+        title: 'Ativar questionário',
+        content: 'Deseja realmente ativar esse questionário?',
+        action: 'ativar',
+      };
+    }
+    const modalRef = this.ngbModal.open(ModalConfirmComponent, {
+      centered: true,
+    });
+    modalRef.componentInstance.modalConfirmData = modalConfirmData;
+    modalRef.result
+      .then((result: any) => {
+        if (result) {
+          this._httpService
+            .post('Quiz/BlockUnblock', value)
+            .pipe(takeUntil(this.onDestroy))
+            .subscribe(
+              (response: any) => {
+                this.megaleiosAlertService.success(response.message);
+                const index = this.list.findIndex(
+                  (item) => item.id === value.targetId
+                );
+                this.list[index].blocked = value.block;
+              },
+              (response: any) => {
+                this.megaleiosAlertService.error(response.message);
+              }
+            );
+        }
+      })
+      .catch(() => {});
+  }
 }
