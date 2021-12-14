cf41666c1f3d6463fbbdc9066cdfeb4c90892cf3
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-14 11:33:22 -0300

    Correção de lógica

--- src/app/utils/modules/videos/containers/videos-list/videos-list.component.ts
+++ src/app/utils/modules/videos/containers/videos-list/videos-list.component.ts
@@ -3,34 +3,33 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ListContainerClass } from 'src/app/utils/classes/list-container.class';
 import { FormDataModel } from 'src/app/utils/functions/generate-form-data.function';
 import { HttpService } from 'src/app/utils/services/http/http.service';
-import {takeUntil} from 'rxjs/operators';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {MegaleiosAlertService} from '../../../megaleios-alert/megaleios-alert.service';
-import {ModalConfirmData} from '../../../shared/components/modal-confirm/modal-confirm.interface';
-import {ModalConfirmComponent} from '../../../shared/components/modal-confirm/modal-confirm.component';
+import { takeUntil } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MegaleiosAlertService } from '../../../megaleios-alert/megaleios-alert.service';
+import { ModalConfirmData } from '../../../shared/components/modal-confirm/modal-confirm.interface';
+import { ModalConfirmComponent } from '../../../shared/components/modal-confirm/modal-confirm.component';
 
 @Component({
   selector: 'app-videos-list',
   templateUrl: './videos-list.component.html',
-  styleUrls: ['./videos-list.component.sass']
+  styleUrls: ['./videos-list.component.sass'],
 })
 export class VideosListComponent extends ListContainerClass {
-
   formDataModel: FormDataModel = {
     columns: [
       { data: 'title', name: 'Title', searchable: true },
       { data: 'startDate', name: 'StartDate', searchable: true },
-      { data: 'endDate', name: 'EndDate', searchable: true }
+      { data: 'endDate', name: 'EndDate', searchable: true },
     ],
     page: 1,
     pageSize: 10,
     search: {
-      search: ''
+      search: '',
     },
     order: {
       column: '0',
-      direction: 'asc'
-    }
+      direction: 'asc',
+    },
   };
 
   uri = 'Video';
@@ -46,36 +45,50 @@ export class VideosListComponent extends ListContainerClass {
   }
 
   handleDetails(): void {
-    this._router.navigate([this.listSelected[0].id], { relativeTo: this._activatedRoute.parent });
+    this._router.navigate([this.listSelected[0].id], {
+      relativeTo: this._activatedRoute.parent,
+    });
   }
 
   handleBlockUnblock(value: any): void {
-
     let modalConfirmData: ModalConfirmData;
     if (value.block) {
       modalConfirmData = {
-        title: 'Remover vídeo',
-        content: 'Deseja realmente remover esse vídeo?',
-        action: 'remover',
+        title: 'Inativar vídeo',
+        content: 'Deseja realmente inativar esse vídeo?',
+        action: 'inativar',
+      };
+    } else {
+      modalConfirmData = {
+        title: 'Ativar vídeo',
+        content: 'Deseja realmente ativar esse vídeo?',
+        action: 'ativar',
       };
     }
-    const modalRef = this.ngbModal.open(ModalConfirmComponent, { centered: true });
+    const modalRef = this.ngbModal.open(ModalConfirmComponent, {
+      centered: true,
+    });
     modalRef.componentInstance.modalConfirmData = modalConfirmData;
     modalRef.result
       .then((result: any) => {
         if (result) {
-          this._httpService.post('Video/BlockUnblock', value)
+          this._httpService
+            .post('Video/BlockUnblock', value)
             .pipe(takeUntil(this.onDestroy))
-            .subscribe((response: any) => {
-              this.megaleiosAlertService.success(response.message);
-              const index = this.list.findIndex((item) => item.id === value.targetId);
-              this.list[index].blocked = value.block;
-            }, (response: any) => {
-              this.megaleiosAlertService.error(response.message);
-            });
+            .subscribe(
+              (response: any) => {
+                this.megaleiosAlertService.success(response.message);
+                const index = this.list.findIndex(
+                  (item) => item.id === value.targetId
+                );
+                this.list[index].blocked = value.block;
+              },
+              (response: any) => {
+                this.megaleiosAlertService.error(response.message);
+              }
+            );
         }
       })
-      .catch(() => { });
+      .catch(() => {});
   }
-
 }
