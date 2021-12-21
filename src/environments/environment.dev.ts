081c04ca2a2eccac2a1f951e4775f8b178dad3d8
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-21 17:33:48 -0300

    Adequações

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   showDefaultLogin: true,
   production: true,
-  baseUrl: 'https://apidev.megaleios.com/ApiMoralarDev',
+  //baseUrl: 'https://apidev.megaleios.com/ApiMoralarDev',
   //baseUrl: 'https://bc49-2804-14c-155-8c23-c5e9-e9ce-5081-3a8.ngrok.io',
-  //baseUrl: 'https://fabricio-localhost.loca.lt',
+  baseUrl: 'https://fabricio-localhost.loca.lt',
 };
