48e8edb205e89e78ffbfc0cf0be9f49742c2e3ca
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-12-21 14:17:33 -0300

    adição de flag

--- src/app/utils/modules/notifications/containers/notifications-list/notifications-list.component.ts
+++ src/app/utils/modules/notifications/containers/notifications-list/notifications-list.component.ts
@@ -24,6 +24,7 @@ export class NotificationsListComponent extends ListContainerClass {
     pageSize: 2,
     search: {
       search: '',
+      forGestor: true,
     },
     order: {
       column: '0',
