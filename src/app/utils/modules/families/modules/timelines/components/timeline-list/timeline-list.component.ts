11e7307da2cffa28882985800afb579576913896
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2022-01-07 17:59:12 -0300

    Adição e coreção de informações

--- src/app/utils/modules/families/modules/timelines/components/timeline-list/timeline-list.component.ts
+++ src/app/utils/modules/families/modules/timelines/components/timeline-list/timeline-list.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
-import { timelineStatusToString } from 'src/app/utils/functions/timelines.function';
+import {
+  timelineStatusToString,
+  timelineTypeSubjectToString,
+} from 'src/app/utils/functions/timelines.function';
 
 @Component({
   selector: 'app-timeline-list',
   templateUrl: './timeline-list.component.html',
-  styleUrls: ['./timeline-list.component.sass']
+  styleUrls: ['./timeline-list.component.sass'],
 })
 export class TimelineListComponent extends ListComponentClass {
-
   timelineStatusToString = timelineStatusToString;
-
+  timelineTypeSubjectToString = timelineTypeSubjectToString;
 }
