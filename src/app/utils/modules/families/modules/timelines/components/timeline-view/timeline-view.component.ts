1bf8667c80b648a2439d69081c34929639784ba3
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2022-01-12 11:30:49 -0300

    Adição de validações iniciais

--- src/app/utils/modules/families/modules/timelines/components/timeline-view/timeline-view.component.ts
+++ src/app/utils/modules/families/modules/timelines/components/timeline-view/timeline-view.component.ts
@@ -1,22 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {HttpService} from '../../../../../../services/http/http.service';
-import {takeUntil} from 'rxjs/operators';
-import {dateAndTimeToString, dateToString} from '../../../../../../functions/date.function';
-import {SCHEDULE_TYPE_LIST} from '../../../../../../interfaces/schedules.interface';
-import {TIMELINE_STATUS_LIST} from '../../../../../../interfaces/timelines.interface';
-import {ModalConfirmData} from '../../../../../shared/components/modal-confirm/modal-confirm.interface';
-import {ModalConfirmComponent} from '../../../../../shared/components/modal-confirm/modal-confirm.component';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {MegaleiosAlertService} from '../../../../../megaleios-alert/megaleios-alert.service';
-import {ListContainerClass} from '../../../../../../classes/list-container.class';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from '../../../../../../services/http/http.service';
+import { takeUntil } from 'rxjs/operators';
+import {
+  dateAndTimeToString,
+  dateToString,
+} from '../../../../../../functions/date.function';
+import { SCHEDULE_TYPE_LIST } from '../../../../../../interfaces/schedules.interface';
+import { TIMELINE_STATUS_LIST } from '../../../../../../interfaces/timelines.interface';
+import { ModalConfirmData } from '../../../../../shared/components/modal-confirm/modal-confirm.interface';
+import { ModalConfirmComponent } from '../../../../../shared/components/modal-confirm/modal-confirm.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MegaleiosAlertService } from '../../../../../megaleios-alert/megaleios-alert.service';
+import { ListContainerClass } from '../../../../../../classes/list-container.class';
 
 @Component({
   selector: 'app-timeline-view',
   templateUrl: './timeline-view.component.html',
-  styleUrls: ['./timeline-view.component.sass']
+  styleUrls: ['./timeline-view.component.sass'],
 })
-export class TimelineViewComponent extends ListContainerClass implements OnInit {
+export class TimelineViewComponent
+  extends ListContainerClass
+  implements OnInit
+{
   timeLineStatusList: any[] = TIMELINE_STATUS_LIST;
   scheduleTypeList: any[] = SCHEDULE_TYPE_LIST;
   listQuizByFamily: any[];
@@ -31,6 +37,9 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
   @Input()
   family: any;
 
+  // novos atributos
+  processoReassentamentoAtivo = false;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private httpService: HttpService,
@@ -40,7 +49,12 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
   ) {
     // @ts-ignore
     super();
-    this.httpService.get(`Family/TimeLineProcessMandatory/${this.activatedRoute.snapshot.paramMap.get('familyId')}`)
+    this.httpService
+      .get(
+        `Family/TimeLineProcessMandatory/${this.activatedRoute.snapshot.paramMap.get(
+          'familyId'
+        )}`
+      )
       .subscribe((response: any) => {
         let indexCurrent = 1;
         this.typeSubjectCurrent = response.data.schedule.typeSubject;
@@ -49,11 +63,16 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
           if (this.stage[i].children.length > 0) {
             let size;
             size = this.stage[i].children.length - 1;
-            this.stage[i].children[size].date = dateAndTimeToString(this.stage[i].children[size].date);
+            this.stage[i].children[size].date = dateAndTimeToString(
+              this.stage[i].children[size].date
+            );
             // checking stages
             switch (this.stage[i].children[size].typeScheduleStatus) {
               case 0:
-                if (this.stage[i].children[size].typeSubject === this.typeSubjectCurrent) {
+                if (
+                  this.stage[i].children[size].typeSubject ===
+                  this.typeSubjectCurrent
+                ) {
                   // current
                   indexCurrent = i;
                   this.tabSelected = this.stage[i].children[size];
@@ -64,7 +83,10 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
                 }
                 break;
               case 4:
-                if (this.stage[i].children[size].typeSubject === this.typeSubjectCurrent) {
+                if (
+                  this.stage[i].children[size].typeSubject ===
+                  this.typeSubjectCurrent
+                ) {
                   // current
                   indexCurrent = i;
                   this.tabSelected = this.stage[i].children[size];
@@ -74,7 +96,6 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
                 }
                 break;
             }
-
           } else {
             this.stage[i].date = dateAndTimeToString(this.stage[i].date);
             // checking stages
@@ -116,46 +137,66 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
       });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   getObjs(current) {
     let link;
     switch (current) {
       case 2:
-        link = `Schedule/DetailTimeLineProcessReunionPGM/${this.activatedRoute.snapshot.paramMap.get('familyId')}`;
+        link = `Schedule/DetailTimeLineProcessReunionPGM/${this.activatedRoute.snapshot.paramMap.get(
+          'familyId'
+        )}`;
         break;
       case 4:
-        link = `Schedule/DetailTimeLineProcessChooseProperty/${this.activatedRoute.snapshot.paramMap.get('familyId')}`;
+        link = `Schedule/DetailTimeLineProcessChooseProperty/${this.activatedRoute.snapshot.paramMap.get(
+          'familyId'
+        )}`;
         break;
       case 7:
       case 8:
-        link = `Schedule/DetailTimeLineProcessChoosePropertyOneAndTwo/${this.activatedRoute.snapshot.paramMap.get('familyId')}/${current}`;
-        this.httpService.get(`Schedule/GetHistoryByFamily/${this.activatedRoute.snapshot.paramMap.get('familyId')}`)
+        link = `Schedule/DetailTimeLineProcessChoosePropertyOneAndTwo/${this.activatedRoute.snapshot.paramMap.get(
+          'familyId'
+        )}/${current}`;
+        this.httpService
+          .get(
+            `Schedule/GetHistoryByFamily/${this.activatedRoute.snapshot.paramMap.get(
+              'familyId'
+            )}`
+          )
           .subscribe((response: any) => {
             this.listSchedulesHistory = response.data;
             for (let i = 0; response.data.length > i; i++) {
-              this.listSchedulesHistory[i].date = dateAndTimeToString(this.listSchedulesHistory[i].date);
+              this.listSchedulesHistory[i].date = dateAndTimeToString(
+                this.listSchedulesHistory[i].date
+              );
             }
           });
         break;
     }
-    this.httpService.get(link)
-      .subscribe((response: any) => {
-        this.listQuizByFamily = response.data.detailQuiz;
-        this.listPropertiesInterest = response.data.interestResidencialProperty;
-        this.listCourseByFamily = response.data.courses;
-        for (let i = 0; response.data.courses.length > i; i++) {
-          this.listCourseByFamily[i].startDate = dateAndTimeToString(this.listCourseByFamily[i].startDate);
-          this.listCourseByFamily[i].endDate = dateAndTimeToString(this.listCourseByFamily[i].endDate);
-        }
-      });
+    this.httpService.get(link).subscribe((response: any) => {
+      this.listQuizByFamily = response.data.detailQuiz;
+      this.listPropertiesInterest = response.data.interestResidencialProperty;
+      this.listCourseByFamily = response.data.courses;
+      for (let i = 0; response.data.courses.length > i; i++) {
+        this.listCourseByFamily[i].startDate = dateAndTimeToString(
+          this.listCourseByFamily[i].startDate
+        );
+        this.listCourseByFamily[i].endDate = dateAndTimeToString(
+          this.listCourseByFamily[i].endDate
+        );
+      }
+    });
   }
 
   activateClass(item) {
     let i: number;
-    for (i = 0; i < document.getElementsByClassName('nav-link active').length; i++) {
-      document.getElementsByClassName('nav-link active')[i].className = 'nav-link';
+    for (
+      i = 0;
+      i < document.getElementsByClassName('nav-link active').length;
+      i++
+    ) {
+      document.getElementsByClassName('nav-link active')[i].className =
+        'nav-link';
     }
     item.target.className = 'nav-link active';
   }
@@ -166,7 +207,7 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
       let size;
       size = this.stage[i].children.length - 1;
       this.tabSelected = this.stage[i].children[size];
-     } else {
+    } else {
       this.tabSelected = this.stage[i];
     }
   }
@@ -179,7 +220,7 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
       typeSubject: 8,
       place: 'Mudança',
       description: 'Mudança',
-      date: value.date
+      date: value.date,
     };
     let modalConfirmData: ModalConfirmData;
 
@@ -189,31 +230,36 @@ export class TimelineViewComponent extends ListContainerClass implements OnInit 
       action: 'confirmar',
     };
 
-    const modalRef = this.ngbModal.open(ModalConfirmComponent, {centered: true});
+    const modalRef = this.ngbModal.open(ModalConfirmComponent, {
+      centered: true,
+    });
     modalRef.componentInstance.modalConfirmData = modalConfirmData;
     modalRef.result
       .then((result: any) => {
         if (result) {
-          this.httpService.post('Schedule/ChangeSubject', post)
-            .subscribe((response: any) => {
+          this.httpService.post('Schedule/ChangeSubject', post).subscribe(
+            (response: any) => {
               this.megaleiosAlertService.success(response.message);
               setTimeout(() => {
                 location.reload();
               }, 1000);
-            }, (response: any) => {
+            },
+            (response: any) => {
               this.megaleiosAlertService.error(response.message);
               setTimeout(() => {
                 location.reload();
               }, 1000);
-            });
+            }
+          );
         }
       })
-      .catch(() => {
-      });
+      .catch(() => {});
   }
 
   handleDetails(): void {
-  this.router.navigate([`/${this.activatedRoute.parent.root.children[0].snapshot.url[0].path}/app/familias/`, this.family.id]);
+    this.router.navigate([
+      `/${this.activatedRoute.parent.root.children[0].snapshot.url[0].path}/app/familias/`,
+      this.family.id,
+    ]);
   }
 }
-
