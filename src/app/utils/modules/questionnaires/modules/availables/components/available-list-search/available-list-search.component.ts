dbc35c8e815bba3450bae99e54f7a2d46c37fa56
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2022-01-19 10:16:37 -0300

    Adição do filtro pelo título do questionário

--- src/app/utils/modules/questionnaires/modules/availables/components/available-list-search/available-list-search.component.ts
+++ src/app/utils/modules/questionnaires/modules/availables/components/available-list-search/available-list-search.component.ts
@@ -17,6 +17,7 @@ export class AvailableListSearchComponent extends ListSearchComponentClass {
       number: [''],
       holderName: [''],
       holderCpf: [''],
+      title: [''],
       status: [''],
     });
   }
