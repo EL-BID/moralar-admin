da0cfd22fc62ab8b930fab963f6019c8935551d8
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2022-01-19 15:43:41 -0300

    Arranjo para teste

--- src/app/utils/modules/courses/components/course-view/course-view.component.ts
+++ src/app/utils/modules/courses/components/course-view/course-view.component.ts
@@ -3,18 +3,19 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-course-view',
   templateUrl: './course-view.component.html',
-  styleUrls: ['./course-view.component.sass']
+  styleUrls: ['./course-view.component.sass'],
 })
 export class CourseViewComponent implements OnChanges {
   @Input()
   course: any;
 
-ngOnChanges(changes: SimpleChanges): void {
-    if(changes?.course?.firstChange)this.course.enrolled = [
-      {fullName: 'Joel dos Santos Silva', cpf: '04967048188', status:0},
-      {fullName: 'Ana dos Santos Silva', cpf: '01067048188', status:0},
-      {fullName: 'Neiely Sara', cpf: '04417048188', status:0}
-    ]
-}
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes.course) {
+      this.course.enrolled = [
+        { fullName: 'Joel dos Santos Silva', cpf: '04967048188', status: 0 },
+        { fullName: 'Ana dos Santos Silva', cpf: '01067048188', status: 0 },
+        { fullName: 'Neiely Sara', cpf: '04417048188', status: 0 },
+      ];
+    }
+  }
 }
