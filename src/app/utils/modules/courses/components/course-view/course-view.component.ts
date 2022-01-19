9db09e0c82b1db05701f54c07ad74043527da321
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2022-01-19 15:21:13 -0300

    Adição de lista de inscritos para teste

--- src/app/utils/modules/courses/components/course-view/course-view.component.ts
+++ src/app/utils/modules/courses/components/course-view/course-view.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-course-view',
   templateUrl: './course-view.component.html',
   styleUrls: ['./course-view.component.sass']
 })
-export class CourseViewComponent {
+export class CourseViewComponent implements OnChanges {
   @Input()
   course: any;
+
+ngOnChanges(changes: SimpleChanges): void {
+    if(changes?.course?.firstChange)this.course.enrolled = [
+      {fullName: 'Joel dos Santos Silva', cpf: '04967048188', status:0},
+      {fullName: 'Ana dos Santos Silva', cpf: '01067048188', status:0},
+      {fullName: 'Neiely Sara', cpf: '04417048188', status:0}
+    ]
+}
 
 }
