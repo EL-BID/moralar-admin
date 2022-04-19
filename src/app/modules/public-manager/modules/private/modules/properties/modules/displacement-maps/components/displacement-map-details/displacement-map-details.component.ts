1e2d2ac4656d7f535f818585ab5e8265e40b9afd
joelwwo <joeldossantossilva21@gmail.com>
2022-04-19 15:12:56 -0300

    Adição de mensagem de erro

--- src/app/modules/public-manager/modules/private/modules/properties/modules/displacement-maps/components/displacement-map-details/displacement-map-details.component.ts
+++ src/app/modules/public-manager/modules/private/modules/properties/modules/displacement-maps/components/displacement-map-details/displacement-map-details.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { HttpService } from './../../../../../../../../../../utils/services/http/http.service';
 
 @Component({
   selector: 'app-displacement-map-details',
   templateUrl: './displacement-map-details.component.html',
-  styleUrls: ['./displacement-map-details.component.sass']
+  styleUrls: ['./displacement-map-details.component.sass'],
 })
 export class DisplacementMapDetailsComponent implements OnInit {
+  family!: any;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
   ngOnInit(): void {
+    this.getFamily();
   }
 
+  getFamily(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.httpService.get(`Family/DisplacementMap/${id}`).subscribe(
+      ({ data }) => (this.family = data),
+      ({ message }) => {
+        Swal.fire(message);
+      }
+    );
+  }
 }
