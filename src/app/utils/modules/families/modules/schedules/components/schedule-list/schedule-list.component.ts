640046a09293e9d3533d1508584b59f1ff14979b
joelwwo <joeldossantossilva21@gmail.com>
2022-05-05 16:04:13 -0300

    Correção da navegação

--- src/app/utils/modules/families/modules/schedules/components/schedule-list/schedule-list.component.ts
+++ src/app/utils/modules/families/modules/schedules/components/schedule-list/schedule-list.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
+
 import {
   SCHEDULE_STATUS_LIST,
-  SCHEDULE_TYPE_LIST
+  SCHEDULE_TYPE_LIST,
 } from '../../../../../../interfaces/schedules.interface';
-
 
 @Component({
   selector: 'app-schedule-list',
   templateUrl: './schedule-list.component.html',
-  styleUrls: ['./schedule-list.component.sass']
+  styleUrls: ['./schedule-list.component.sass'],
 })
 export class ScheduleListComponent extends ListComponentClass {
-   scheduleStatusList = SCHEDULE_STATUS_LIST;
-   scheduleTypeList = SCHEDULE_TYPE_LIST;
+  scheduleStatusList = SCHEDULE_STATUS_LIST;
+  scheduleTypeList = SCHEDULE_TYPE_LIST;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+
+    private router: Router
+  ) {
+    super();
+  }
+
+  handleDetails(id: string): void {
+    this.router.navigate([
+      `/${this.activatedRoute.parent.root.children[0].snapshot.url[0].path}/app/agendamentos`,
+      id,
+    ]);
+  }
 }
