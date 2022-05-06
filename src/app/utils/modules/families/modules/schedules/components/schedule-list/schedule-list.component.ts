42784c4473cafc824be79d76162b842637878fe5
joelwwo <joeldossantossilva21@gmail.com>
2022-05-06 11:02:44 -0300

    Melhoria da lógica

--- src/app/utils/modules/families/modules/schedules/components/schedule-list/schedule-list.component.ts
+++ src/app/utils/modules/families/modules/schedules/components/schedule-list/schedule-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ListComponentClass } from 'src/app/utils/classes/list-component.class';
+import Swal from 'sweetalert2';
 
 import {
   SCHEDULE_STATUS_LIST,
@@ -15,6 +16,7 @@ import {
 export class ScheduleListComponent extends ListComponentClass {
   scheduleStatusList = SCHEDULE_STATUS_LIST;
   scheduleTypeList = SCHEDULE_TYPE_LIST;
+  @Output() finish: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -29,5 +31,17 @@ export class ScheduleListComponent extends ListComponentClass {
       `/${this.activatedRoute.parent.root.children[0].snapshot.url[0].path}/app/agendamentos`,
       id,
     ]);
+  }
+
+  confirmEnd(index: number, schedule: any): void {
+    Swal.fire({
+      text: `Deseja finalizar esse agendamento?`,
+      cancelButtonText: 'Não, cancelar',
+      confirmButtonText: 'Sim, finalizar',
+      showCancelButton: true,
+      reverseButtons: true,
+    }).then(({ isConfirmed }: any) => {
+      if (isConfirmed) this.finish.emit({ index, ...schedule });
+    });
   }
 }
