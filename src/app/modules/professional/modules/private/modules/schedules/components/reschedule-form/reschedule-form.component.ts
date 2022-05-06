78c35899e12cb2a65f61c60e38f4651602fe53fb
joelwwo <joeldossantossilva21@gmail.com>
2022-05-06 18:03:31 -0300

    ADição do questionário na tela de reagendamento

--- src/app/modules/professional/modules/private/modules/schedules/components/reschedule-form/reschedule-form.component.ts
+++ src/app/modules/professional/modules/private/modules/schedules/components/reschedule-form/reschedule-form.component.ts
@@ -1,23 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DateTime } from 'luxon';
 import { FormComponentClass } from 'src/app/utils/classes/form-component.class';
 import { SCHEDULE_TYPE_LIST } from 'src/app/utils/interfaces/schedules.interface';
-import {trimWhiteSpace} from '../../../../../../../../utils/functions/validators.function';
-import {dateAndTimeToString, dateToSeconds, dateToString} from '../../../../../../../../utils/functions/date.function';
+import { trimWhiteSpace } from '../../../../../../../../utils/functions/validators.function';
+import {
+  dateAndTimeToString,
+  dateToSeconds,
+  dateToString,
+} from '../../../../../../../../utils/functions/date.function';
+import {
+  FormDataModel,
+  generateFormData,
+} from 'src/app/utils/functions/generate-form-data.function';
+import { HttpService } from 'src/app/utils/services/http/http.service';
+import { MegaleiosAlertService } from 'src/app/utils/modules/megaleios-alert/megaleios-alert.service';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-reschedule-form',
   templateUrl: './reschedule-form.component.html',
-  styleUrls: ['./reschedule-form.component.sass']
+  styleUrls: ['./reschedule-form.component.sass'],
 })
-export class RescheduleFormComponent extends FormComponentClass implements OnInit{
+export class RescheduleFormComponent
+  extends FormComponentClass
+  implements OnInit
+{
   scheduleTypeList: any[] = SCHEDULE_TYPE_LIST;
   dayMin = DateTime.local()
     .set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
     .toSeconds();
 
+  formDataModelQuiz: FormDataModel = {
+    columns: [{ data: 'number', name: 'Number', searchable: false }],
+    page: 1,
+    pageSize: 10,
+    search: {
+      search: '',
+      number: '',
+      holderName: '',
+      holderCpf: '',
+      status: '',
+    },
+    order: {
+      column: '0',
+      direction: 'asc',
+    },
+  };
+
+  questionnaires = [];
+
   constructor(
-    formBuilder: FormBuilder
+    formBuilder: FormBuilder,
+    private httpService: HttpService,
+    private megaleiosAlertService: MegaleiosAlertService
   ) {
     super();
     this.form = formBuilder.group({
@@ -27,12 +62,35 @@ export class RescheduleFormComponent extends FormComponentClass implements OnIni
       place: [null, Validators.compose([trimWhiteSpace, Validators.required])],
       description: [null, Validators.required],
       time: [null, Validators.required],
+      quiz: [{ disabled: true, value: null }, Validators.required],
     });
+    this.form
+      .get('typeSubject')
+      .valueChanges.subscribe((typeSubject: number) => {
+        if (typeSubject == 7) this.form.get('quiz').enable();
+        else this.form.get('quiz').enable();
+      });
   }
 
   ngOnInit(): void {
-    this.form.controls.time.setValue(dateAndTimeToString(+this.form.controls.date.value).substr(-5, 5));
-    this.form.controls.date.setValue(dateToSeconds(dateToString(+this.form.controls.date.value)));
+    this.form.controls.time.setValue(
+      dateAndTimeToString(+this.form.controls.date.value).substr(-5, 5)
+    );
+    this.form.controls.date.setValue(
+      dateToSeconds(dateToString(+this.form.controls.date.value))
+    );
+    this.getQuestionnaires();
   }
 
+  getQuestionnaires(): void {
+    this.httpService
+      .post(`Quiz/LoadData`, generateFormData(this.formDataModelQuiz))
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(
+        ({ data }: any) => {
+          this.questionnaires = data;
+        },
+        ({ message }: any) => this.megaleiosAlertService.error(message)
+      );
+  }
 }
