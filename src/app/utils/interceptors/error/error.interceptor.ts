49f7d2381faeaa16d371273994e848badbbc4202
joelwwo <joeldossantossilva21@gmail.com>
2022-05-09 16:54:35 -0300

    Correção de undefined

--- src/app/utils/interceptors/error/error.interceptor.ts
+++ src/app/utils/interceptors/error/error.interceptor.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
 import { AuthenticationService } from 'src/app/utils/services/authentication/authentication.service';
 import { HttpService } from 'src/app/utils/services/http/http.service';
 import { Router } from '@angular/router';
@@ -8,36 +13,47 @@ import { catchError, tap, flatMap } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
   constructor(
     private authenticationService: AuthenticationService,
     private httpService: HttpService,
     private router: Router
-  ) { }
+  ) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req)
-      .pipe(
-        catchError((responseOne: any) => {
-          if (responseOne.status === 401) {
-            const value = { refreshToken: this.authenticationService.getAuthentication().refresh_token };
-            return this.httpService.post('Profile/Token', value)
-              .pipe(
-                tap(() => this.authenticationService.unsetAuthentication()),
-                flatMap((responseTwo: any) => {
-                  this.authenticationService.setAuthentication(responseTwo.data, false);
-                  req = req.clone({ setHeaders: { Authorization: `bearer ${responseTwo.data.access_token}` } });
-                  return next.handle(req);
-                }),
-                catchError((responseTwo: any) => {
-                  this.router.navigate(['/']);
-                  return throwError(responseTwo.error);
-                })
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((responseOne: any) => {
+        if (responseOne.status === 401) {
+          const token = this.authenticationService.getAuthentication();
+          if (!token?.refresh_token)
+            this.authenticationService.unsetAuthentication();
+          const value = {
+            refreshToken: token?.refresh_token,
+          };
+          return this.httpService.post('Profile/Token', value).pipe(
+            tap(() => this.authenticationService.unsetAuthentication()),
+            flatMap((responseTwo: any) => {
+              this.authenticationService.setAuthentication(
+                responseTwo.data,
+                false
               );
-          }
-          return throwError(responseOne.error || responseOne);
-        })
-      );
+              req = req.clone({
+                setHeaders: {
+                  Authorization: `bearer ${responseTwo.data.access_token}`,
+                },
+              });
+              return next.handle(req);
+            }),
+            catchError((responseTwo: any) => {
+              this.router.navigate(['/']);
+              return throwError(responseTwo.error);
+            })
+          );
+        }
+        return throwError(responseOne.error || responseOne);
+      })
+    );
   }
-
 }
