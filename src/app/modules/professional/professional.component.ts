1d0eb161266cfbcc055880134da60bd75584737b
joelwwo <joeldossantossilva21@gmail.com>
2022-06-22 11:29:49 -0300

    Adição dos títulos dinâmicos

--- src/app/modules/professional/professional.component.ts
+++ src/app/modules/professional/professional.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-professional',
   templateUrl: './professional.component.html',
-  styleUrls: ['./professional.component.sass']
+  styleUrls: ['./professional.component.sass'],
 })
-export class ProfessionalComponent { }
+export class ProfessionalComponent {
+  constructor(readonly titleService: Title) {
+    titleService.setTitle('Profissional');
+  }
+}
--- src/app/modules/public-manager/public-manager.component.ts
+++ src/app/modules/public-manager/public-manager.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-public-manager',
   templateUrl: './public-manager.component.html',
-  styleUrls: ['./public-manager.component.sass']
+  styleUrls: ['./public-manager.component.sass'],
 })
-export class PublicManagerComponent { }
+export class PublicManagerComponent {
+  constructor(readonly titleService: Title) {
+    titleService.setTitle('Gestor Público');
+  }
+}
